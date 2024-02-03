8611f091b34bed14df5a8f911be04b028924e409
simone0404 <simoneroldo04@gmail.com>
2024-02-03 14:30:01 +0100

    Simo

--- main.py
+++ main.py
@@ -1,6 +1,61 @@
 import praw
 import pandas as pd
 from pydub import AudioSegment
+
+import sottotitoli as st
+import tiktok
+
+
+def dividi_testo(text, num_parole=45):
+    parole = text.strip().split()
+    num = len(parole) // num_parole + 1
+    sottostringhe = []
+    for i in range(num):
+        sottostringhe.append(' '.join(parole[i * num_parole:(i + 1) * num_parole]))
+    return sottostringhe
+
+
+def from_reddit_to_posts():
+    reddit = praw.Reddit(
+        client_id="HU4BpzSxzqNStBb3X-lfeQ",
+        client_secret="l_-UaJpNj6BpTUyMm-8apYb9vC0fCg",
+        user_agent="android:com.example.myredditapp:v1.2.3 (by u/PitifulBook4011)",
+    )
+
+    posts = []
+    ml_subreddit = reddit.subreddit('offmychest+relationships+amitheasshole')
+    for post in ml_subreddit.top(time_filter='week', limit=1):
+        posts.append(
+            [post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
+    posts = pd.DataFrame(posts, columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
+    return posts
+
+
+def from_posts_to_mp3(lista_posts):
+    for t in range(1):
+        listastr = dividi_testo(lista_posts.iloc[t].loc['body'])
+        output = AudioSegment.empty()
+        for blocco in listastr:
+            audio_segment = tiktok.tts("f133bd730fc2e44ad33cf5bda762c6fc", "en_us_006", blocco)
+            output += audio_segment
+        output.export(f'./output/Post {t}.mp3', format="mp3")
+
+        st.sottotitoli(f"./output/Post {t}.mp3")
+
+
+def main():
+    posts = from_reddit_to_posts()
+    posts.to_csv('file1.csv')
+
+    from_posts_to_mp3(posts)
+
+
+if __name__ == "__main__":
+    main()
+
+"""import praw
+import pandas as pd
+from pydub import AudioSegment
 import sottotitoli as st
 
 import tiktok
@@ -13,6 +68,15 @@ def contatoreparole(string):
         if i == " ":
             cont += 1
     return cont
+
+
+def dividi_testo(text, num_parole=45):
+    parole = text.strip().split()
+    num = len(parole) // num_parole + 1
+    sottostringhe = []
+    for i in range(num):
+        sottostringhe.append(' '.join(parole[i * num_parole:(i + 1) * num_parole]))
+    return sottostringhe
 
 
 def main():
@@ -31,9 +95,17 @@ def main():
 
     posts.to_csv('file1.csv')
 
-    # posts.iloc[i][6]
-    arraysuoni = []
     for t in range(10):
+        listastr = dividi_testo(posts.iloc[t].iloc[6])
+        output = AudioSegment.empty()
+        for blocco in listastr:
+            audio_segment = tiktok.tts("f133bd730fc2e44ad33cf5bda762c6fc", "en_us_006", blocco)
+            output += audio_segment
+
+        output.export(f'./output/Post {t}.mp3', format="mp3")
+
+        st.sottotitoli(f"./output/Post {t}.mp3")
+        for t in range(10):
 
         parole = contatoreparole(posts.iloc[t].iloc[6])
         n = 0
@@ -54,8 +126,6 @@ def main():
                 listacaratteri.append(k)
             listastr.append(listacaratteri.copy())
 
-        w = 0
-
         output = AudioSegment.empty()
 
         for p in listastr:
@@ -68,12 +138,6 @@ def main():
         st.sottotitoli(f"./output/Post {t}.mp3")
 
 
-
-
-
 if __name__ == "__main__":
     main()
-
-
-# tiktok.tts("f133bd730fc2e44ad33cf5bda762c6fc","en_us_006",stringa,f'Post - 1 .mp3',False)
-
+"""
