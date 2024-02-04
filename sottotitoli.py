b1ee8a8e5f4da8f3b784218056317914932dd820
simone0404 <simoneroldo04@gmail.com>
2024-02-04 10:53:48 +0100

    QuasiFatto

--- sottotitoli.py
+++ sottotitoli.py
@@ -1,39 +1,38 @@
-from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
-from pydub import AudioSegment
-import subprocess
-import whisper
-import librosa
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip, AudioFileClip
+import random
+import os
 
 
-def sottotitoli(audio_file_path: str):
-    # Definisci il percorso del modello di lingua italiano per Whisper
+def format_string(s):
+    words = s.split()
+    formatted = '\n'.join([' '.join(words[i:i + 3]) for i in range(0, len(words), 3)])
+    return formatted
 
-    audio = whisper.load_audio(audio_file_path)
-    model = whisper.load_model("small")
-    transcriptions = model.transcribe(audio)
-    words = [entry["word"] for entry in transcriptions if "word" in entry]
 
-    print(words)
-
-    audio_duration = librosa.get_duration(filename=audio_file_path)
+def video_sottotitoli(audio_file_path: str, lista_str_and_dur: (str, int), num_posts: int):
     # Carica il video
-    video_path = "minecraft.mp4"
-    video = VideoFileClip(video_path).set_duration(audio_duration)
+    audio_duration = sum(dur[1] for dur in lista_str_and_dur)
+    starting_point = random.randint(0, 300)
+    print(f"Video started at second {starting_point}")
+    video = VideoFileClip("minecraft.mp4").subclip(starting_point)
+    video = video.set_duration(audio_duration)
 
-    text_clips = [
-        TextClip(word, fontsize=24, color='white', bg_color="", size=(video.w, video.h))
-        .set_position(('center', 'bottom'))
-        .set_duration(1)
-        for word in words
-    ]
+    text_clips = []
+    start_time = 0
+    for elem in lista_str_and_dur:
+        text_clips.append(
+            TextClip(format_string(elem[0]), fontsize=15, color='white', font='Impact', size=(video.w, video.h))
+            .set_position(('center', 'bottom'))
+            .set_start(start_time)
+            .set_duration(elem[1]))
+
+        start_time += elem[1]
 
     # Sovrapponi le clip di testo al video
     result = CompositeVideoClip([video.set_duration(audio_duration)] + text_clips, size=(video.w, video.h))
-
+    result = result.set_audio(AudioFileClip(audio_file_path))
     # Salva il video risultante
-    result.write_videofile("video_with_subtitles.mp4", codec="libx264", audio_codec="aac")
+    result.write_videofile(f"./output/video_with_audio {num_posts}.mp4", codec="libx264", audio_codec="aac")
 
     video.reader.close()
-
-if __name__ == "__main__":
-    sottotitoli("./output/Post 0.mp3")
+    os.remove(f'./output/Post {num_posts}.mp3')
