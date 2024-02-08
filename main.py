b4aeb66e436c9d3ea4457190395e1174e934251b
simone0404 <simoneroldo04@gmail.com>
2024-02-08 12:03:56 +0100

    modificheTitolo

--- main.py
+++ main.py
@@ -2,35 +2,37 @@ import praw
 import pandas as pd
 import pydub
 import re
-import upload
 
 import sottotitoli as st
 import tiktok
+import upload
 
 
 def divide_text(text: str):
-    char_to_split = "[({}).:;!?]"
-    frasi = re.split(char_to_split, text.strip())
+    char_to_split = "[({}).:;!?]"  # sono quelli interni alle []
+    frasi = re.split(char_to_split, text.strip())  # divisione testo per punteggiatura
 
     frasi.append("Press the like button, comment with your opinion")
     frasi.append("Share the video and subscribe to the channel")
 
     sentences = []
     for block in frasi:
-        blocks = block.split()
-        if(len(blocks) < 40):
+        blocks = block.split()  # divisione per parole
+        if len(blocks) < 40:  # caso accettato da tiktok
             sentences.append(block)
             continue
         piece = len(blocks) // 40 + 1
         already_taken = 0
         for i in range(piece):
             if i == piece - 1:
-                sentences.append(' '.join(blocks[already_taken:]))
+                sentences.append(' '.join(
+                    blocks[already_taken:]))  # l'ultimo blocco della frase è unito indipendentemente dalla lunghezza
             else:
-                sentences.append(' '.join(blocks[already_taken: already_taken + len(blocks) // piece]))
+                sentences.append(' '.join(blocks[already_taken: already_taken + len(
+                    blocks) // piece]))  # unione parole in più pezzi accettati da tiktok
                 already_taken += len(blocks) // piece
-
     return sentences
+
 
 def from_reddit_to_posts(number_of_posts):
     reddit = praw.Reddit(
@@ -48,52 +50,64 @@ def from_reddit_to_posts(number_of_posts):
     return posts
 
 
-def from_posts_to_mp3(lista_posts):
-    num_post = 0
+def from_posts_to_video(lista_posts):
+    with open("numerazione_posts.txt", "r") as file:
+        testo_file = file.readlines()
+        parti = testo_file[0].split(":")
+
+    parti[1] = parti[1].replace(" ", "")
+    num_post = int(parti[1])
     chars_read_by_tiktok = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
                             "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
                             "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    for t in range(len(lista_posts)):
+        listastr = divide_text(lista_posts.iloc[t].loc['body'])  # listastr è il testo del post
+        output = pydub.AudioSegment.empty()
+        lista_str_and_dur = []  # considera tuple (str, int) = (schermata sottotitolo; durata sottotitolo)
+        for block in listastr:
+            if not set(block.lower()).intersection(chars_read_by_tiktok):  # controlla caratteri leggibili
+                continue
+            if block == '':  # controlla stringa vuota
+                continue
+            response = tiktok.tts("f133bd730fc2e44ad33cf5bda762c6fc", "en_us_006",
+                                  block)  # ottiene dizionario con file audio
+            if response["status_code"] == 5:  # se audio_segment non corretto passa al post successivo
+                break
 
-    def from_posts_to_mp3(lista_posts):
-        num_post = 0
-        chars_read_by_tiktok = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-                                "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
-                                "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-        for t in range(len(lista_posts)):
-            listastr = divide_text(lista_posts.iloc[t].loc['body'])
-            output = pydub.AudioSegment.empty()
-            lista_str_and_dur = []
-            for block in listastr:
-                if not set(block.lower()).intersection(chars_read_by_tiktok):
-                    continue
-                if block == '':
-                    continue
-                response = tiktok.tts("f133bd730fc2e44ad33cf5bda762c6fc", "en_us_006", block)
-                if response["status_code"] == 5:  # se audio_segment non corretto passa al post successivo
-                    break
-                block = block.split()
-                piece = len(block) // 9 + 1
-                already_taken = 0
-                for i in range(piece):
-                    if i == piece - 1:
-                        subtitle = ' '.join(block[already_taken:])
-                    else:
-                        subtitle = ' '.join(block[already_taken: already_taken + len(block) // piece])
-                    already_taken += len(block) // piece
-                    lista_str_and_dur.append((subtitle, response["audio_segment"].duration_seconds / piece))
-                output += response["audio_segment"]
-            else:  # eseguito se non sono stati prodotti audio_segment scorrett
-                output = output.speedup(playback_speed=1.5)
-                output.export(f'./output/Post {num_post}.mp3', format="mp3")
-                st.video_sottotitoli(f"./output/Post {num_post}.mp3", lista_str_and_dur, num_post)
-                num_post += 1
+            # creazione di lista_str_and_dur dividendo la frase in più blocchi di 9 parole
+            block = block.split()
+            piece = len(block) // 9 + 1
+            already_taken = 0
+            for i in range(piece):
+                if i == piece - 1:
+                    subtitle = ' '.join(block[already_taken:])
+                else:
+                    subtitle = ' '.join(block[already_taken: already_taken + len(block) // piece])
+                already_taken += len(block) // piece
+                lista_str_and_dur.append((subtitle, response["audio_segment"].duration_seconds / piece / 1.15))
+
+            output += response["audio_segment"]  # concatena i file audio
+        else:  # eseguito se non sono stati prodotti audio_segment scorretti
+            video_title = lista_posts.iloc[t].loc["title"]
+            video_title = video_title.replace(".", "")
+            output = output.speedup(playback_speed=1.15)
+            output.export(f'./output/Post {num_post}.mp3', format="mp3")
+            st.video_sottotitoli(f"./output/Post {num_post}.mp3", lista_str_and_dur, num_post, video_title)
+            num_post += 1
+
+    parti[1] = parti[1].replace(f"{parti[1]}", str(num_post), 1)
+    with open("numerazione_posts.txt", "w") as file:
+        parti[1] = f" {parti[1]}\n"
+        first_line = ":".join(parti)
+        testo_file[0] = first_line
+        file.writelines(testo_file)
 
 
 def main():
     number_of_posts = 1
     posts = from_reddit_to_posts(number_of_posts)
     posts.to_csv('file1.csv')
-    from_posts_to_mp3(posts)
+    from_posts_to_video(posts)
 
 
 if __name__ == "__main__":
--- sottotitoli.py
+++ sottotitoli.py
@@ -9,21 +9,21 @@ def format_string(s):
     return formatted
 
 
-def video_sottotitoli(audio_file_path: str, lista_str_and_dur: (str, int), num_posts: int):
+def video_sottotitoli(audio_file_path: str, lista_str_and_dur: (str, int), num_posts: int, video_title: str):
     # Carica il video
-    audio_duration = sum(dur[1] for dur in lista_str_and_dur)
+    audio_duration = sum(elem[1] for elem in lista_str_and_dur)
     starting_point = random.randint(15, 300)
     print(f"Video started at second {starting_point}")
     video = VideoFileClip("minecraft.mp4").subclip(starting_point)
     video = video.set_duration(audio_duration)
-    video = video.resize(height=1920)  # Ridimensiona l'altezza a 1920
-    video = video.crop(x_center=video.w / 2, width=1080)
+    # video = video.resize(height=1920)  # Ridimensiona l'altezza a 1920
+    # video = video.crop(x_center=video.w / 2, width=1080)
 
     text_clips = []
     start_time = 0
     for elem in lista_str_and_dur:
         text_clips.append(
-            TextClip(format_string(elem[0]), fontsize=20, color='white', font='Impact', size=(1080, 1920))
+            TextClip(format_string(elem[0]), fontsize=30, color='white', font='Impact', size=(video.w, video.h), transparent=True)
             .set_position(('center', 'bottom'))
             .set_start(start_time)
             .set_duration(elem[1]))
@@ -31,10 +31,10 @@ def video_sottotitoli(audio_file_path: str, lista_str_and_dur: (str, int), num_p
         start_time += elem[1]
 
     # Sovrapponi le clip di testo al video
-    result = CompositeVideoClip([video.set_duration(audio_duration)] + text_clips, size=(1080, 1920))
+    result = CompositeVideoClip([video.set_duration(audio_duration)] + text_clips, size=(video.w, video.h)) 
     result = result.set_audio(AudioFileClip(audio_file_path))
     # Salva il video risultante
-    result.write_videofile(f"./output/video_with_audio {num_posts}.mp4", codec="libx264", audio_codec="aac")
+    result.write_videofile(f"./output/{num_posts}, {video_title}.mp4", codec="libx264", audio_codec="aac")
 
     video.reader.close()
     os.remove(f'./output/Post {num_posts}.mp3')
